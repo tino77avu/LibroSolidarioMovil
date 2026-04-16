665a2b9a22c301a41f5b762afdac1a002a681b12
INNCode <N_Code_IN@hotmail.com>
2026-04-16 17:08:58 -0500

    Add splash screen support with new theme and update MainActivity for splash screen integration
    - Introduced a new splash screen theme in themes.xml.
    - Updated AndroidManifest.xml to apply the new splash theme to MainActivity.
    - Integrated the splash screen in MainActivity.java.
    - Added core splash screen dependency in build.gradle.kts.
    - Updated libs.versions.toml to include core splash screen version.

--- app/src/main/java/com/example/librosolidario/MainActivity.java
+++ app/src/main/java/com/example/librosolidario/MainActivity.java
@@ -6,6 +6,7 @@ import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.splashscreen.SplashScreen;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -16,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.installSplashScreen(this);
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
